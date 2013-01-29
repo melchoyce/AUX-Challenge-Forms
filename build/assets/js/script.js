70f1ee4ca5c927baa7535689afc21fe20236f9d3
Mel Choyce <melissachoyce@gmail.com>
2013-01-29 18:15:44 -0500

    Working more on js

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -8,18 +8,22 @@ function getCreditCardType(accountNumber) {
   if (/^5[1-5]/.test(accountNumber)) { // Check for mastercard
   
     result = "mastercard";
-  
+    document.getElementById("#card-icons").className = "mastercard";
+
   } else if (/^4/.test(accountNumber)) { // Check for visa
  
     result = "visa";
+    document.getElementById("#card-icons").className = "visa";
   
   } else if (/^3[47]/.test(accountNumber)) { // Check for amex
   
     result = "amex";
+    document.getElementById("#card-icons").className = "amex";
   
-  } else if (/^6011-?/.text(accountNumber)) {
+  } else if (/^6011-?/.text(accountNumber)) { // Check for discover
 
     result="discover";
+    document.getElementById("#card-icons").className = "discover";
 
   }
 
