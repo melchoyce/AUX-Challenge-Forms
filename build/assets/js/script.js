62b6416fa1eb4474ed6fdb9458975e789e94a4e1
Mel Choyce <melissachoyce@gmail.com>
2013-01-31 16:44:09 -0500

    Including modernizr

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -1,77 +1,53 @@
-// Find out what numbers people have entered
-// Change the background position of the card that matches those numbers
-// Bind this to a keyup event
+(function () {
 
+    function displayCardType() {
+        // Get credit card value and strip all non-numerical characters
+        var cardNum = document.getElementById('card-num').value.replace(/\D/g,'');
 
+        // Save all of the icons into variables
+        var amex = document.getElementById('amex'),
+            visa = document.getElementById('visa'),
+            discover = document.getElementById('discover'),
+            mastercard = document.getElementById('mastercard'),
+            securityIcon = document.getElementById('security-icon');
 
-/* Function to figure out what kind of credit card a user is entering */
+        // Reset position values
+        amex.nextSibling.style.backgroundPosition = '0 -42px';
+        visa.nextSibling.style.backgroundPosition = '-48px -42px';
+        discover.nextSibling.style.backgroundPosition = '-96px -42px';
+        mastercard.nextSibling.style.backgroundPosition = '-144px -42px';
+        securityIcon.style.backgroundPosition = '-80px -84px';
 
-function getCreditCardType(accountNumber) {
+        // Check input against card requirements
+        if (/^3[47]/.test(cardNum)) { // American Express start with 34 or 37 and have 15 digits
 
-  //start without knowing the credit card type
-  var result = "unknown";
+            amex.nextSibling.style.backgroundPosition = '0 0';
+            securityIcon.style.cssText = 'background-position: 0 -84px; width: 70px; height: 33px;';
 
-  if (/^5[1-5]/.test(accountNumber)) { // Check for mastercard
-  
-    result = "mastercard";
-    // document.getElementById("#card-icons").className = "mastercard";
+        } else if (/^4/.test(cardNum)) { // Visa card numbers start with a 4
 
-  } else if (/^4/.test(accountNumber)) { // Check for visa
- 
-    result = "visa";
-    // document.getElementById("#card-icons").className = "visa";
-  
-  } else if (/^3[47]/.test(accountNumber)) { // Check for amex
-  
-    result = "amex";
-    // document.getElementById("#card-icons").className = "amex";
-  
-  } else if (/^6/.text(accountNumber)) { // Check for discover
+            visa.nextSibling.style.backgroundPosition = '-48px 0';
 
-    result="discover";
-    // document.getElementById("#card-icons").className = "discover";
+        } else if (/^6/.test(cardNum)) { // Discover start with 6011 or 65. All have 16 digits
 
-  }
+            discover.nextSibling.style.backgroundPosition = '-96px 0';
 
-  return result;
+        } else if (/^5[1-5]/.test(cardNum)) { // MasterCard start with 51 through 55 and have 16 digits
 
-}
+            mastercard.nextSibling.style.backgroundPosition = '-144px 0';
 
-function handleEvent(event)
-{
-  var value   = event.target.value,
-      type    = getCreditCardType(value);
+        }
 
-  switch (type)
-  {
-    case "mastercard":
-        //show MasterCard icon
-        document.getElementById("#card-icons").className = "mastercard";
-        break;
+    }
 
-    case "visa":
-        //show Visa icon
-        document.getElementById("#card-icons").className = "visa";
-        break;
+    // get the input from the card-num field every time someone presses a key
+    document.getElementById('card-num').addEventListener('keyup', displayCardType);
 
-    case "amex":
-        //show American Express icon
-        document.getElementById("#card-icons").className = "amex";
-        break;
+})();
 
-    case "discover":
-        document.getElementById("#card-icons").className = "discover";
-        break;
-
-    default:
-        //clear all icons?
-        //show error?
-  }
-}
-
-// or window.onload
-document.addEventListener("DOMContentLoaded", function(){
-  var textbox = document.getElementById("card-num");
-  textbox.addEventListener("keyup", handleEvent, false);
-  textbox.addEventListener("blur", handleEvent, false);
-}, false);
+Modernizr.load(
+    {
+        test : Modernizr.input.required,
+        nope : ['http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js', 'http://ajax.aspnetcdn.com/ajax/jquery.validate/1.10.0/jquery.validate.min.js', '/AUX-Challenge-Forms/build/assets/js/validate.js']
+    }
+);
