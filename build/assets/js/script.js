8557ec03d73136ebe62d9ad9649d01589624df18
Mel Choyce <melissachoyce@gmail.com>
2013-01-31 14:05:47 -0500

    Removing index.php

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -1,3 +1,9 @@
+// Find out what numbers people have entered
+// Change the background position of the card that matches those numbers
+// Bind this to a keyup event
+
+
+
 /* Function to figure out what kind of credit card a user is entering */
 
 function getCreditCardType(accountNumber) {
@@ -8,27 +14,64 @@ function getCreditCardType(accountNumber) {
   if (/^5[1-5]/.test(accountNumber)) { // Check for mastercard
   
     result = "mastercard";
-    document.getElementById("#card-icons").className = "mastercard";
+    // document.getElementById("#card-icons").className = "mastercard";
 
   } else if (/^4/.test(accountNumber)) { // Check for visa
  
     result = "visa";
-    document.getElementById("#card-icons").className = "visa";
+    // document.getElementById("#card-icons").className = "visa";
   
   } else if (/^3[47]/.test(accountNumber)) { // Check for amex
   
     result = "amex";
-    document.getElementById("#card-icons").className = "amex";
+    // document.getElementById("#card-icons").className = "amex";
   
-  } else if (/^6011-?/.text(accountNumber)) { // Check for discover
+  } else if (/^6/.text(accountNumber)) { // Check for discover
 
     result="discover";
-    document.getElementById("#card-icons").className = "discover";
+    // document.getElementById("#card-icons").className = "discover";
 
   }
 
-  return result;  
+  return result;
 
 }
 
-// capture event + attach function to it
+function handleEvent(event)
+{
+  var value   = event.target.value,
+      type    = getCreditCardType(value);
+
+  switch (type)
+  {
+    case "mastercard":
+        //show MasterCard icon
+        document.getElementById("#card-icons").className = "mastercard";
+        break;
+
+    case "visa":
+        //show Visa icon
+        document.getElementById("#card-icons").className = "visa";
+        break;
+
+    case "amex":
+        //show American Express icon
+        document.getElementById("#card-icons").className = "amex";
+        break;
+
+    case "discover":
+        document.getElementById("#card-icons").className = "discover";
+        break;
+
+    default:
+        //clear all icons?
+        //show error?
+  }
+}
+
+// or window.onload
+document.addEventListener("DOMContentLoaded", function(){
+  var textbox = document.getElementById("card-num");
+  textbox.addEventListener("keyup", handleEvent, false);
+  textbox.addEventListener("blur", handleEvent, false);
+}, false);
