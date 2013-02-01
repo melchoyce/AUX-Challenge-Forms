1f907940361504a9814bd602c9d1c89b349c6457
Mel Choyce <melissachoyce@gmail.com>
2013-02-01 11:15:48 -0500

    Changing function
    Making displayCardType use :checked instead of changing the background
    position

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -11,30 +11,23 @@
             mastercard = document.getElementById('mastercard'),
             securityIcon = document.getElementById('security-icon');
 
-        // Reset position values
-        amex.nextSibling.style.backgroundPosition = '0 -42px';
-        visa.nextSibling.style.backgroundPosition = '-48px -42px';
-        discover.nextSibling.style.backgroundPosition = '-96px -42px';
-        mastercard.nextSibling.style.backgroundPosition = '-144px -42px';
-        securityIcon.style.backgroundPosition = '-80px -84px';
-
         // Check input against card requirements
         if (/^3[47]/.test(cardNum)) { // American Express start with 34 or 37 and have 15 digits
 
-            amex.nextSibling.style.backgroundPosition = '0 0';
+            amex.checked = "true";
             securityIcon.style.cssText = 'background-position: 0 -84px; width: 70px; height: 33px;';
 
         } else if (/^4/.test(cardNum)) { // Visa card numbers start with a 4
 
-            visa.nextSibling.style.backgroundPosition = '-48px 0';
+            visa.checked = "true";
 
         } else if (/^6/.test(cardNum)) { // Discover start with 6011 or 65. All have 16 digits
 
-            discover.nextSibling.style.backgroundPosition = '-96px 0';
+            discover.checked = "true";
 
         } else if (/^5[1-5]/.test(cardNum)) { // MasterCard start with 51 through 55 and have 16 digits
 
-            mastercard.nextSibling.style.backgroundPosition = '-144px 0';
+            mastercard.checked = "true";
 
         }
 
